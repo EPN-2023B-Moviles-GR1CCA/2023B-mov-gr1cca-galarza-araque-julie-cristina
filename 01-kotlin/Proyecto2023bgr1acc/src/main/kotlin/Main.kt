49c78a7547b61ed7f6b1b07e221fb699431d81ef
Julie Galarza <julie.galarza@epn.edu.ec>
2023-11-16 17:46:12 -0500

    Add main.kt clases

--- 01-kotlin/Proyecto2023bgr1acc/src/main/kotlin/Main.kt
+++ 01-kotlin/Proyecto2023bgr1acc/src/main/kotlin/Main.kt
@@ -1,35 +1,142 @@
-import java.util.Date
+import java.util.*
 
 fun main(){
     println("Hola mundo")
-    //INMUTABLEES ( NO se reasignan "=")
+    // INMUTABLES (NO se reasignan "=")
     val inmutable: String = "Adrian";
-    //inmutable = "Vicente";
+    // inmutable = "Vicente";
 
-    //Mutable (Re asignar)
-    var mutable: String = "Vecente";
+    // Mutables (Re asignar)
+    var mutable: String = "Vicente";
     mutable = "Adrian";
 
-    //Variable primitiva
-    val nombreProfesor: String="Adrian Eguez"
+
+
+
+
+
+
+
+
+
+    //  val > var
+    // Duck Typing
+    var ejemploVariable = " Adrian Eguez "
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo;
+
+    // Variable primitiva
+    val nombreProfesor: String = "Adrian Eguez"
     val sueldo: Double = 1.2
     val estadoCivil: Char = 'C'
     val mayorEdad: Boolean = true
-    //Clases Java
+    // Clases Java
     val fechaNacimiento: Date = Date()
 
-    //SWITCH
+
+    // SWITCH
     val estadoCivilWhen = "C"
     when (estadoCivilWhen){
         ("C") -> {
             println("Casado")
         }
-        "S" ->{
+        "S" -> {
             println("Soltero")
         }
-        else ->{
+        else -> {
             println("No sabemos")
         }
     }
-    val coqueteo = if(estadoCivilWhen == "Si") else "No"
+    val coqueteo = if (estadoCivilWhen == "S") "Si" else "No"
+
+    calcularSueldo(10.00)
+    calcularSueldo(10.00, 15.00)
+    calcularSueldo(10.00, 12.00, 20.00)
+    //parametros nombrados
+    calcularSueldo(sueldo = 10.00)
+    calcularSueldo(sueldo = 10.00, tasa = 15.00)
+    calcularSueldo(sueldo = 10.00, tasa = 12.00, bonoEspecial = 20.00)
+
+    calcularSueldo(sueldo = 10.00, bonoEspecial = 20.00)//Named parameters
+    calcularSueldo(10.00, bonoEspecial = 20.00)//Named parameters
+
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00)//parametros nombrados
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+abstract class NumerosJava{
+    protected  val numeroUno:Int
+    private val numeroDos: Int
+
+    constructor(
+        uno: Int,
+        dos: Int
+    ){//Bloque de codigo del constructor
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+
+
+
+
+
+abstract class Numeros(//Constructor PRIMARIO
+    //Ejemplo:
+    //unoProp: Init, //(Parametro(sin modificacidor de acceso))
+    //private var uno: Int,//propiedad Publica Clase numeros.uno
+    //var uno: Int,
+    //Propiedasd de la clase protected numeros.numeroUno
+    protected val numeroUno:Int,
+    //propiedad de la clase protected numeros.numeroDos
+    protected val numeroDos:Int,
+){
+    //var cedula:string= ""(public es por defecto)
+    //private valorCalculado: Int = 0 (private)
+    init {//Bloque codigo constructor primario
+        this.numeroUno; this.numeroDos; // this es opcional
+        numeroUno; numeroDos; // sin el "this", es lo mismo
+        println("Inicializando")
+    }
+}
+
+
+
+
+// void -> Unit
+fun imprimirNombre(nombre: String): Unit{
+    // "Nombre: " + variable + " bienvenido";
+    println("Nombre : ${nombre}")
+}
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null, // Opcion null -> nullable
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        bonoEspecial.dec()
+        return sueldo * (100/tasa ) + bonoEspecial
+    }
 }
