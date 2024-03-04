2c545f176136574819181a7caa897a5356a7f752
juliegalarza <julie.galarza@epn.edu.ec>
2024-03-04 11:11:31 -0500

    Examen IIB

--- Deber 02/2023B-mov-gr1cca-galarza-araque-julie-cristina/02-android/app/src/main/java/com/example/gr1accjcga2023b/EBaseDeDatos.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gr1accjcga2023b
-
-import com.example.gr1accvaes2023b.ESqliteHelperEntrenador
-
-class EBaseDeDatos {
-    companion object{
-        var tablaEntrenador: ESqliteHelperEntrenador? = null
-    }
-}
